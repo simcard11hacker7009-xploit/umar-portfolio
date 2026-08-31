0e272ba71443c56c22ce8da2936328d437e8386d
simcard11hacker7009-xploit <simcard11hacker7009@gmail.com>
2026-08-31 15:41:29 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 // Send contact messages to Gmail
 const transporter = nodemailer.createTransport({
